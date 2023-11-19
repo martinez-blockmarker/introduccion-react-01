371bb1059003d3b79ceb780203829c2f3575daed
Ainoha Armesto Martinez <lainos.aa@gmail.com>
2023-11-19 20:46:24 +0100

    añadir estructura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import Ejercicio1 from './components/Ejercicio1/Ejercicio1'
+import Ejercicio1 from './components/Ejercicio1'
+import Ejercicio2 from './components/Ejercicio2'
+import Ejercicio3 from './components/Ejercicio3'
+import Ejercicio4 from './components/Ejercicio4'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TituloPrincipal from './components/TituloPrincipal'
 import './index.css'
+import main from 'componets./main'
 
-export default function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <TituloPrincipal />
-        <Ejercicio1 />
-        {/* Añade tus componentes aqui */}
-      </main>
-      <Footer />
-    </>
+
+export default function App(){
+return 
+  (
+  <><TituloPrincipal /><Ejercicio1 /><Ejercicio2 /><Ejercicio3 /><Ejercicio4 /></></>
   )
 }
+
+
+
--- src/components/Ejercicio1/Ejercicio1.jsx
+++ src/components/Ejercicio1/Ejercicio1.jsx
@@ -1,10 +1,11 @@
-import TituloEjercicio from '../TituloEjercicio'
+import Saludar from './Saludar'
+import Saludar from '.Saludar'
 
 export default function Ejercicio1() {
   return (
     <section className="caja-ejercicio">
       <TituloEjercicio>Este es el ejercicio 1</TituloEjercicio>
-      {/* Añade el resto del ejercicio aqui */}
+      <Saludar nombre = "Soy Batman"/>
     </section>
   )
 }
